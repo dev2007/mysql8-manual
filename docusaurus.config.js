3ed295b9c0af5a12a82c7865d41841d0fbfd78b3
dev2007 <softengineerwu@gmail.com>
2023-01-28 13:48:14 +0800

    modify onBrokenLink

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'Dinosaurs are cool',
   url: 'https://mysql.bookhub.tech',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
